ef79065a5308a02143c4075ec33d807a831b653a
Amit <amit.p@expertappdevs.dev>
2025-11-19 13:13:40 +0530

    changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,13 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-   const {data:blogs,error,loading} = useFetch('http://localhost:8000/blogs')
+   // Use json-server in development if available, otherwise use static file
+   // For Vercel deployment, this will use the static /blogs.json file
+   const apiUrl = import.meta.env.DEV && import.meta.env.VITE_USE_JSON_SERVER === 'true'
+     ? 'http://localhost:8000/blogs'
+     : '/blogs.json';
+
+   const {data:blogs,error,loading} = useFetch(apiUrl)
 
     return (<div className="home">
         {error && <div>{error}</div>}
